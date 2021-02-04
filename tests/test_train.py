89b9dc3b9d232702c3e401e4afaf2ab7709f54c3
Peter Yu <2057325+yukw777@users.noreply.github.com>
2021-02-03 19:31:43 -0500

    upgrade PL to 1.1.7 and hydra to 1.0.6
    Remove logger tests and the default logger is now Tensorboard.

--- tests/test_train.py
+++ tests/test_train.py
@@ -2,63 +2,13 @@ import pytest
 import sys
 
 from hydra.experimental import initialize, compose
-from omegaconf import open_dict
-from pytorch_lightning.loggers import TensorBoardLogger, WandbLogger
 
 from leela_zero_pytorch.train import main as train_main
 from leela_zero_pytorch.weights import main as weights_main
 
 
-@pytest.mark.parametrize(
-    "logger",
-    [[], ["~logger"]],
-)
-def test_train_logger(monkeypatch, tmp_path, capsys, logger):
-    with initialize(config_path="../leela_zero_pytorch/conf"):
-        cfg = compose(
-            config_name="config",
-            overrides=[
-                "+network=small",
-                "data.train_data_dir=tests/test-data",
-                "data.train_dataloader_conf.batch_size=2",
-                "data.val_data_dir=tests/test-data",
-                "data.val_dataloader_conf.batch_size=2",
-                "data.test_data_dir=tests/test-data",
-                "data.test_dataloader_conf.batch_size=2",
-                f"+pl_trainer.default_root_dir={tmp_path}",
-                "+pl_trainer.fast_dev_run=true",
-            ]
-            + logger,
-        )
-        if len(logger) == 0:
-            # default wandb logger
-            with open_dict(cfg):
-                cfg.logger.offline = True
-                cfg.logger.save_dir = str(tmp_path)
-        with capsys.disabled():
-            # both pytest and wandb capture stdout, and they cause
-            # a deadlock, so don't capture when running the trainer
-            trainer = train_main(cfg)
-
-    checkpoint_path = "checkpoints/epoch=0.ckpt"
-    if isinstance(trainer.logger, TensorBoardLogger):
-        checkpoint_path = f"{trainer.logger.log_dir}/{checkpoint_path}"
-    elif isinstance(trainer.logger, WandbLogger):
-        checkpoint_path = (
-            f"{tmp_path}/{trainer.logger.name}/"
-            f"{trainer.logger.version}/{checkpoint_path}"
-        )
-
-    monkeypatch.setattr(
-        sys,
-        "argv",
-        ["lzp-weights", checkpoint_path, f"{tmp_path}/weights.txt"],
-    )
-    weights_main()
-
-
-@pytest.mark.parametrize("network_size", ["small", "big", "huge"])
-def test_train_network_size(monkeypatch, tmp_path, capsys, network_size):
+@pytest.mark.parametrize("network_size", ["small"])
+def test_train_network_size(network_size):
     with initialize(config_path="../leela_zero_pytorch/conf"):
         cfg = compose(
             config_name="config",
@@ -70,21 +20,20 @@ def test_train_network_size(monkeypatch, tmp_path, capsys, network_size):
                 "data.val_dataloader_conf.batch_size=2",
                 "data.test_data_dir=tests/test-data",
                 "data.test_dataloader_conf.batch_size=2",
-                f"+pl_trainer.default_root_dir={tmp_path}",
                 "+pl_trainer.fast_dev_run=true",
-                "~logger",
             ],
         )
-        with capsys.disabled():
-            # both pytest and wandb capture stdout, and they cause
-            # a deadlock, so don't capture when running the trainer
-            trainer = train_main(cfg)
+        train_main(cfg)
 
-    checkpoint_path = f"{trainer.logger.log_dir}/checkpoints/epoch=0.ckpt"
 
+def test_lzp_weights(monkeypatch, tmp_path):
     monkeypatch.setattr(
         sys,
         "argv",
-        ["lzp-weights", checkpoint_path, f"{tmp_path}/weights.txt"],
+        [
+            "lzp-weights",
+            "tests/test-data/epoch=0-step=0.ckpt",
+            f"{tmp_path}/weights.txt",
+        ],
     )
     weights_main()
